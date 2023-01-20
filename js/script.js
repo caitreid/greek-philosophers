46d932a963f7ee3845837a4985c73a3a50fe0e0a
caitreid <ctlnrd@gmail.com>
2023-01-20 02:23:00 -0500

    show hide based on whether its on view in museum

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const modals = document.querySelectorAll('[data-modal]');
 
-modals.forEach(function (trigger) {
-  trigger.addEventListener('click', function (event) {
+modals.forEach((trigger) => {
+  trigger.addEventListener('click', (event) => {
     event.preventDefault();
     const modal = document.getElementById(trigger.dataset.modal);
     modal.classList.add('open');
     const exits = modal.querySelectorAll('.modal__exit');
-    exits.forEach(function (exit) {
+    exits.forEach((exit) => {
       exit.addEventListener('click', function (event) {
         event.preventDefault();
         modal.classList.remove('open');
@@ -16,4 +16,35 @@ modals.forEach(function (trigger) {
 });
 
 
-console.log('attached')
+
+const onViewButton = document.querySelector('.filter__button')
+const cards = document.querySelectorAll('.card')
+
+onViewButton.addEventListener('click', (event) => {
+    if (!onViewButton.classList.contains('clicked')){
+        event.preventDefault();
+
+        console.log('hit') 
+
+        cards.forEach(card => {
+
+            if (card.dataset.onView === "false") {
+                card.classList.add('hide')
+                console.log('hidden ')
+
+                onViewButton.classList.add('clicked')
+            }
+        })
+
+        onViewButton.innerHTML = "<button class='view-all'>View all</button>"
+
+    } else {
+        console.log('hide')
+        onViewButton.classList.remove('clicked')
+        cards.forEach(card =>  card.classList.remove('hide'))
+        onViewButton.innerHTML = "<button>On view</button>"
+    }
+})
+
+
+
